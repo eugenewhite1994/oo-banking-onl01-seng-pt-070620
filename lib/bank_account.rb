09a5ab65c14a66fb8c71c03d96b49ab5a7c45223
Eugene White <eugenewhite1994@gmail.com>
2020-08-10 23:00:16 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,12 @@
 class BankAccount
-attr_accessor :balance, :status, :deposit
+attr_accessor :balance, :status
 attr_reader :name 
 
 def initialize(name)
   @name = name
   @balance = balance 
   @status = status
-  @deposit = deposit
 end
-end
+
+def display_balance(balance)
+puts balance
